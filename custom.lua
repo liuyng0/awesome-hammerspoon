6a9059d6a744b93998f0baa5de26e75c4ae24e6c
Yang Liu <ayliu@amazon.com>
2017-08-25 10:19:57 +0800

    Format.
    Signed-off-by: Yang Liu <ayliu@amazon.com>

--- custom.lua
+++ custom.lua
@@ -1,51 +1,45 @@
 -- Specify Spoons which will be loaded
 hspoon_list = {
-    "AClock",
-    "BingDaily",
-    -- "Calendar",
-    "CircleClock",
-    "ClipShow",
-    "CountDown",
-    "FnMate",
-    "HCalendar",
-    "HSaria2",
-    "HSearch",
-    -- "KSheet",
-    "SpeedMenu",
-    -- "TimeFlow",
-    -- "UnsplashZ",
-    "WinWin",
+   "AClock",
+   "BingDaily",
+   -- "Calendar",
+   "CircleClock",
+   "ClipShow",
+   "CountDown",
+   "FnMate",
+   "HCalendar",
+   "HSaria2",
+   "HSearch",
+   -- "KSheet",
+   "SpeedMenu",
+   -- "TimeFlow",
+   -- "UnsplashZ",
+   "WinWin",
 }
 
 -- appM environment keybindings. Bundle `id` is prefered, but application `name` will be ok.
 hsapp_list = {
-    {key = 'a', name = 'Atom'},
-    {key = 'c', id = 'com.google.Chrome'},
-    {key = 'd', name = 'ShadowsocksX'},
-    {key = 'e', name = 'Emacs'},
-    {key = 'f', name = 'Finder'},
-    {key = 'i', name = 'iTerm'},
-    {key = 'k', name = 'KeyCastr'},
-    {key = 'l', name = 'Sublime Text'},
-    {key = 'm', name = 'MacVim'},
-    {key = 'o', name = 'LibreOffice'},
-    {key = 'p', name = 'mpv'},
-    {key = 'r', name = 'VimR'},
-    {key = 's', name = 'Safari'},
-    {key = 't', name = 'Terminal'},
-    {key = 'v', id = 'com.apple.ActivityMonitor'},
-    {key = 'w', name = 'Mweb'},
-    {key = 'y', id = 'com.apple.systempreferences'},
+   {key = 'e', name = 'Emacs'},
+   {key = 'i', name = 'iTerm'},
+   {key = 'c', id = 'com.google.Chrome'},
+   {key = 'f', name = 'Finder'},
+   {key = 'd', name = 'ShadowsocksX'},
+   {key = 'v', id = 'com.apple.ActivityMonitor'},
+   {key = 'k', name = 'KeyCastr'},
+   {key = 'y', id = 'com.apple.systempreferences'},
 }
 
+hyper1 = {"ctrl", "shift", "alt"}
+hyper2 = "cmd"
+
 -- Modal supervisor keybinding, which can be used to temporarily disable ALL modal environments.
-hsupervisor_keys = {{"alt", "shift", "ctrl"}, "Q"}
+hsupervisor_keys = {hyper1, "Q"}
 
 -- Reload Hammerspoon configuration
-hsreload_keys = {{"alt", "shift", "ctrl"}, "R"}
+hsreload_keys = {hyper1, "R"}
 
 -- Toggle help panel of this configuration.
-hshelp_keys = {{"alt", "shift", "ctrl"}, "/"}
+hshelp_keys = {hyper1, "/"}
 
 -- aria2 RPC host address
 hsaria2_host = "http://localhost:6800/jsonrpc"
@@ -60,11 +54,11 @@ hswhints_keys = {"alt", "tab"}
 
 -- appM environment keybinding: Application Launcher
 -- hsappM_keys = {"alt", "A"}
-hsappM_keys = {{"alt", "shift", "ctrl"}, "L"}
+hsappM_keys = {hyper2, "L"}
 
 -- clipshowM environment keybinding: System clipboard reader
 -- hsclipsM_keys = {"alt", "C"}
-hsclipsM_keys = {{"alt", "shift", "ctrl"}, "C"}
+hsclipsM_keys = {hyper1, "C"}
 
 -- Toggle the display of aria2 frontend
 -- hsaria2_keys = {"alt", "D"}
@@ -72,23 +66,23 @@ hsaria2_keys = {"", ""}
 
 -- Launch Hammerspoon Search
 -- hsearch_keys = {"alt", "G"}
-hsearch_keys = {{"alt", "shift", "ctrl"}, "S"}
+hsearch_keys = {hyper1, "S"}
 
 -- Read Hammerspoon and Spoons API manual in default browser
 -- hsman_keys = {"alt", "H"}
-hsman_keys = {{"alt", "shift", "ctrl"}, "H"}
+hsman_keys = {hyper1, "H"}
 
 -- countdownM environment keybinding: Visual countdown
 -- hscountdM_keys = {"alt", "I"}
-hscountdM_keys = {{"alt", "shift", "ctrl"}, "I"}
+hscountdM_keys = {hyper1, "I"}
 
 -- Lock computer's screen
 -- hslock_keys = {"alt", "L"}
-hslock_keys = {{"alt", "shift", "ctrl"}, "."}
+hslock_keys = {hyper1, "."}
 
 -- resizeM environment keybinding: Windows manipulation
 -- hsresizeM_keys = {"alt", "R"}
-hsresizeM_keys = {{"alt", "shift", "ctrl"}, "W"}
+hsresizeM_keys = {hyper2, "M"}
 
 -- cheatsheetM environment keybinding: Cheatsheet copycat
 -- hscheats_keys = {"alt", "S"}
@@ -96,7 +90,7 @@ hscheats_keys = {"", ""}
 
 -- Show digital clock above all windows
 -- hsaclock_keys = {"alt", "T"}
-hsaclock_keys = {{"alt", "shift", "ctrl"}, "T"}
+hsaclock_keys = {hyper1, "T"}
 
 -- Type the URL and title of the frontmost web page open in Google Chrome or Safari.
 -- hstype_keys = {"alt", "V"}
@@ -104,4 +98,4 @@ hstype_keys = {"", ""}
 
 -- Toggle Hammerspoon console
 -- hsconsole_keys = {"alt", "Z"}
-hsconsole_keys = {{"alt", "shift", "ctrl"}, "C"}
+hsconsole_keys = {hyper1, "C"}
