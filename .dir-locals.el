531ea385d9864cdbc71bee6e006e52b0b33cf97e
chophi <chophi@foxmail.com>
2024-02-19 11:34:39 +0800

    add emmylua annotation config

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+((lua-mode . ((eval .
+                    (setq-local
+                       lsp-lua-workspace-library
+                                  (ht ("~/.hammerspoon/Spoons/EmmyLua.spoon/annotations" t)))))
+               ))
+)
